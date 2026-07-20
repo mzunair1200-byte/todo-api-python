21c42877a54557620aa5a30d3ed30242dcc2980f
mzunair1200-byte <mzunair1200@gmail.com>
2026-07-20 14:05:36 +0500

    Stage 2: read endpoints with 404

--- main.py
+++ main.py
@@ -15,4 +15,24 @@ def health():
     return {
         "status":"ok"
     }
+# 1.our in memory database with three exmaple tasks
+Task=[
+    {"id":1,"title":"Task 1","description":"This is task 1","completed":False},
+    {"id":2,"title":"Task 2","description":"This is task 2","completed":False},
+    {"id":3,"title":"Task 3","description":"This is task 3","completed":False}
+]
+@app.get("/task")
+def get_task():
+    return Task
 
+@app.get("/task/{task_id}")
+def get_task_by_id(task_id:int):    
+    
+    for task in Task:
+        if task["id"]==task_id:
+            return task
+    return {"error":"Task not found"}
+
+@app.get("/healthcheck")
+def healthcheck():
+    return {"status":"ok"}
